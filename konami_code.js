7fb13b847b10a7ec63fc82f352af77289a92dd26
Haruka Matsumoto <matsumoto.haru@gmail.com>
2017-12-27 01:45:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,10 +7,10 @@ let index = 0;
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
 
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
 
-    if (index === alphabet.length) {
+    if (index === code.length) {
       window.alert("Hurray!");
 
       index = 0;
